c5f44f819bb4febca429eeb377e8b643416ffb8c
eeminov1995 <e.eminov1995@mail.ru>
2023-01-19 17:04:43 +0300

    Add folder
    - feat: Создали папку

---
+++ 2.3.sep_end_переменные_PEP_8/2.3.5.py
@@ -0,0 +1,14 @@
+# I like Python
+# Напишите программу, которая выводит на экран текст «I***like***Python» (без кавычек).
+
+# Примечание. Используйте необязательный параметр sep.
+
+# Тестовые данные 
+# Sample Input:
+
+# Sample Output:
+
+# I***like***Python
+
+# put your python code here
+print("I", "like", "Python", sep="***")

d51c4cd749d5a045c7aeba8955a2defd3c1c7d6f
eeminov1995 <122723463+eeminov1995@users.noreply.github.com>
2023-01-21 21:29:16 +0300

    Delete 7.2.Цикл_for:_функция_range directory

--- 7.2.Цикл_for:_функция_range/7.2.11.py
+++
@@ -1,6 +0,0 @@
-# Таблица умножения
-
-n = int(input())
-
-for i in range(1, 11):
-    print(f'{n} x {i} = {i * n}')
--- 7.2.Цикл_for:_функция_range/7.2.8.py
+++
@@ -1,7 +0,0 @@
-# Последовательность чисел 2 🌶️
-
-m = int(input())
-n = int(input())
-
-for i in range(m, n + 1) or range(m, n - 1, -1):
-    print(i)

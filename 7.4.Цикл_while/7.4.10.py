4e57c7c867bdffb3ffef51d8838b0f1ae53cd438
eeminov1995 <e.eminov1995@mail.ru>
2023-01-25 18:47:56 +0300

    Edit file
    - feat: Изменили файлы

---
+++ 7.4.Цикл_while/7.4.10.py
@@ -0,0 +1,6 @@
+text = input()
+i = 0
+while text != 'стоп' and text != 'хватит' and text != 'достаточно':
+    text = input()
+    i += 1
+print(i)
---
+++ 7.4.Цикл_while/7.4.13.py
@@ -0,0 +1,7 @@
+num = int(input())
+i = 0
+while 0 <= num <= 5:
+    if num == 5:
+        i += 1
+    num = int(input())
+print(i)
